af2222047a67ac6912b62382ec023431e15a98c6
amul <wrokatamul07@gmail.com>
2024-11-14 07:46:42 +0530

    Change module name from hospital_system to hospital_base
    - Renamed the module from hospital_system to hospital_base.
    - Updated the overall module structure for better organization and scalability.
    - Added a new file, patent.py, and created a Many2one relationship for improved data management.

--- hospital_base/models/appointment.py
+++ hospital_base/models/appointment.py
@@ -3,6 +3,6 @@ from odoo import models, fields, api
 
 class hospitalAppointment(models.Model):
     _name = 'patient.appointment'
-    _description = 'Hospital Appointment'
+    _description = 'Hospital Patient Appointment'
 
     patient_id = fields.Many2one('hospital.patient', string="Patient Name")
