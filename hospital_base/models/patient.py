2d9b584f9e9c8a732f2b1e4897e1a4443b41016c
amul <wrokatamul07@gmail.com>
2024-12-01 16:54:45 +0530

    [ADD] Compute method create on Birthdate to age calculate

--- hospital_base/models/patient.py
+++ hospital_base/models/patient.py
@@ -1,15 +1,16 @@
-from odoo import models, fields, api
+from odoo import api, fields, models
+from datetime import date
 
-
-class hospital(models.Model):
+class HospitalPatient(models.Model):
     _name = 'hospital.patient'
     _description = 'Hospital Patient'
     _inherit = ['mail.thread', 'mail.activity.mixin']
 
     name = fields.Char(string="Patient's full name", required=True, tracking=True)
-    age =  fields.Integer(string="Patient's age", tracking=True)
-    gender = fields.Selection(selection=[('male', 'Male'),('female', 'Female'),('other', 'Other'),],string="Gender", tracking=True)
-    blood_type = fields.Selection(selection=[('a+', 'A+'),('a-', 'A-'),('b+', 'B+'),('b-', 'B+'),('o+', 'O+'),('o-', 'O+'),('ab+', 'AB+'),('ab-', 'AB-'),], tracking=True)
+    age = fields.Integer(string="Patient's age", compute="_compute_age", tracking=True)
+    gender = fields.Selection(selection=[('male', 'Male'),('female', 'Female'),('other', 'Other')], string="Gender", tracking=True)
+    date_of_birth = fields.Date("Date Of Birth")
+    blood_type = fields.Selection(selection=[('a+', 'A+'),('a-', 'A-'),('b+', 'B+'),('b-', 'B+'),('o+', 'O+'),('o-', 'O+'),('ab+', 'AB+'),('ab-', 'AB-')], tracking=True)
     contact_number = fields.Char(string="Phone Numbers", tracking=True)
     email = fields.Char(string="Email", tracking=True)
     address = fields.Html(string="Address", tracking=True)
@@ -17,3 +18,14 @@ class hospital(models.Model):
     is_emergency_case = fields.Boolean(string="Is Emergency Case", tracking=True)
     active = fields.Boolean(string="Active", default=True)
 
+    @api.depends("date_of_birth")
+    def _compute_age(self):
+        print("self________________________",self)
+        today = date.today()
+        for record in self:
+            print("record______________",record)
+            if record.date_of_birth:
+                age = today.year - record.date_of_birth.year - ((today.month, today.day) < (record.date_of_birth.month, record.date_of_birth.day))
+                record.age = age
+            else:
+                record.age = 0 
