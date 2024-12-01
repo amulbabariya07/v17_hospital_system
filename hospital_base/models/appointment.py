1974fd5641698d45128b79defb3e19eb356b0166
amul <wrokatamul07@gmail.com>
2024-12-01 16:55:59 +0530

    [ADD] Onchnage method add in Genderchange based select on Patient Name

--- hospital_base/models/appointment.py
+++ hospital_base/models/appointment.py
@@ -1,9 +1,13 @@
 from odoo import models, fields, api
 
-
 class hospitalAppointment(models.Model):
     _name = 'patient.appointment'
     _description = 'Hospital Patient Appointment'
 
     patient_id = fields.Many2one('hospital.patient', string="Patient Name")
-    gender = fields.Selection(selection=[('male', 'Male'),('female', 'Female'),('other', 'Other'),],string="Gender", related='patient_id.gender')
+    gender = fields.Selection(selection=[('male', 'Male'),('female', 'Female'),('other', 'Other'),],string="Gender")
+
+    @api.onchange('patient_id')
+    def _onchange_patient(self):
+        if self.patient_id:
+            self.gender = self.patient_id.gender
