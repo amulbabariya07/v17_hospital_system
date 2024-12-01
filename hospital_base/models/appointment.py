547b22f374ad890af1deee86585aadb6b8127599
amul <wrokatamul07@gmail.com>
2024-12-01 16:14:55 +0530

    [ADD] Set a Related Field in Appointment View from Patient View

--- hospital_base/models/appointment.py
+++ hospital_base/models/appointment.py
@@ -6,3 +6,4 @@ class hospitalAppointment(models.Model):
     _description = 'Hospital Patient Appointment'
 
     patient_id = fields.Many2one('hospital.patient', string="Patient Name")
+    gender = fields.Selection(selection=[('male', 'Male'),('female', 'Female'),('other', 'Other'),],string="Gender", related='patient_id.gender')
